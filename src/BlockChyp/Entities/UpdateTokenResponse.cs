c37609c42a1a4fa36ef005a0e5c7a1c51243ace2
BlockChyp SDK Builder <devops@blockchyp.com>
2025-10-06 21:03:34 +0000

    Merge pull request #342 from blockchyp/task/VIP-473
    Task/vip 473 - Added the updateToken route to the SDKs

---
+++ src/BlockChyp/Entities/UpdateTokenResponse.cs
@@ -0,0 +1,40 @@
+// Copyright 2019-2025 BlockChyp, Inc. All rights reserved. Use of this code is
+// governed by a license that can be found in the LICENSE file.
+//
+// This file was generated automatically by the BlockChyp SDK Generator. Changes
+// to this file will be lost every time the code is regenerated.
+
+using Newtonsoft.Json;
+
+namespace BlockChyp.Entities
+{
+    /// <summary>
+    /// The response to a update token request.
+    /// </summary>
+    public class UpdateTokenResponse : BaseEntity, IAbstractAcknowledgement
+    {
+        /// <summary>
+        /// Whether or not the request succeeded.
+        /// </summary>
+        [JsonProperty(PropertyName = "success")]
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// The error, if an error occurred.
+        /// </summary>
+        [JsonProperty(PropertyName = "error")]
+        public string Error { get; set; }
+
+        /// <summary>
+        /// A narrative description of the transaction result.
+        /// </summary>
+        [JsonProperty(PropertyName = "responseDescription")]
+        public string ResponseDescription { get; set; }
+
+        /// <summary>
+        /// The updated token for a given query.
+        /// </summary>
+        [JsonProperty(PropertyName = "token")]
+        public CustomerToken Token { get; set; }
+    }
+}
